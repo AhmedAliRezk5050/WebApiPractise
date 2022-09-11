6405e6f44c81b6902825ded57b9ed62c13cf69a3
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-09-11 19:37:04 +0200

    initial commit - api scaffold

---
+++ WebApiPractise/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiPractise.Models;
+
+namespace WebApiPractise
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(
+              DbContextOptions<DataContext> opts) : base(opts)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; } = null!;
+    }
+}
---
+++ WebApiPractise/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiPractise;
+namespace WebApiPractise.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
